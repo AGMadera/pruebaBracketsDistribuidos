f1243c8a3379b698fa2b5b908c2c44b1613808a4
AGMadera <ponchp.agm@gmail.com>
2016-01-22 14:26:48 -0600

    subi todo, solo falta la tabla y lo quiero hacer en angular porque soy rechipotles!!

---
+++ ejercicio21Enero/ejercicio1.php
@@ -0,0 +1,25 @@
+<?php
+
+$tabla=5;
+$inicio=6;
+$fin=10;
+$inicio2=16;
+
+echo "inicio en 6 y termino en 10 <br>";
+while($inicio<=$fin){
+        $tem=$tabla*$inicio;
+        echo "$tabla*$inicio=$tem <br>";
+        $inicio++;
+
+}
+
+
+echo "inicio en 16 y termino en 10 <br>";
+while($inicio2>=$fin){
+        $tem=$tabla*$inicio2;
+        echo "$tabla*$inicio2=$tem <br>";
+        $inicio2--;
+
+}
+
+?>
